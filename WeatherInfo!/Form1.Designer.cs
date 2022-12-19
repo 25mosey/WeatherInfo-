5e85ac185c3acd330df4c2a9f0f17e7523c19a20
Juria1005 <shami1987@yandex.ru>
2022-12-19 16:31:31 +0200

    Добавьте файлы проекта.

---
+++ WeatherInfo!/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WeatherInfo_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Town1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Жмяк";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(531, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Температура в заданном городе";
+            // 
+            // Town1
+            // 
+            this.Town1.Location = new System.Drawing.Point(37, 162);
+            this.Town1.Multiline = true;
+            this.Town1.Name = "Town1";
+            this.Town1.Size = new System.Drawing.Size(162, 52);
+            this.Town1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите название города или страны";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(499, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(289, 91);
+            this.label3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Town1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Town1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WeatherInfo!/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeatherSeti;
+
+namespace WeatherInfo_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string url = "https://api.openweathermap.org/data/2.5/weather?q=" + Town1.Text + "&units=metric&appid=a7389f0a3c8042bd42ca476a2c4a4821";
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string response;
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+            WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response);
+            label3.Text=String.Format("Temperature in {0}: {1} °C", weatherResponse.Name, weatherResponse.Main.Temp);
+            
+        }
+    }
+}
